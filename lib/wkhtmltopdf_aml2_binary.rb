8e0d0b0f501c5397eb1ecb3cd558505d1f0452ac
anwar <mac.mem65@gmail.com>
2021-02-22 17:04:04 +0200

    removed wicked_pdf path config

--- lib/wkhtmltopdf_aml2_binary.rb
+++ lib/wkhtmltopdf_aml2_binary.rb
@@ -1,12 +1,1 @@
 # frozen_string_literal: true
-
-begin
-  require 'wicked_pdf'
-  WickedPdf.config ||= {}
-
-  executable_path = "#{Gem.loaded_specs['wkhtmltopdf-aml2-binary'].full_gem_path}/bin/wkhtmltopdf-linux-amd64"
-
-  WickedPdf.config[:exe_path] = executable_path
-rescue StandardError
-  # Ignored
-end
